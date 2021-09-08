0a977c26d2753fed874870b8fcdfe1319bba1812
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2021-09-08 17:14:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $("html").on("click", function () {
-  document.getElementById("demo").innerHTML = "<a href='https://www.spotify.com/us/duo/invite/8z03Xb6Bx4yzb45/'> click to get a spotify here bby <3 </a>";
+  document.getElementById("demo").innerHTML = "<a href='https://www.spotify.com/us/duo/invite/8z03Xb6Bx4yzb45/'> click here to get a spotify bby <3 </a>";
   $(this).toggleClass("on");
 });
 
