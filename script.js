611ced85f30c421b4255d459cf1bae7a7ae27393
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2021-09-08 15:46:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ $("html").on("click", function () {
   document.getElementById("demo").innerHTML = "bby, I love you, and you're my bestfriend <3";
   $(this).toggleClass("on");
 });
-setTimeout(function () {
-  $("html").addClass("on");
-}, 1000);
+
 
