4152fe3f2ebdcac2771b95e737602e3ff5c0152f
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2021-09-08 15:55:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $("html").on("click", function () {
-  document.getElementById("demo").innerHTML = "https://www.youtube.com/watch?v=-mmlQ2HPfXE";
+  document.getElementById("demo").innerHTML = '<a href=https://' + www.youtube.com/watch?v=-mmlQ2HPfXE + '>find out here!</a>';
   $(this).toggleClass("on");
 });
 
