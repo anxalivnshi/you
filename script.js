ba024eeae3adf761c3045a5cc435f7bfe70b0a9c
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2021-09-08 15:57:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $("html").on("click", function () {
-  document.getElementById("demo").innerHTML = '<a href=https://' + www.youtube.com/watch?v=-mmlQ2HPfXE + '>find out here!</a>';
+  document.getElementById("demo").innerHTML = '<a href=https://' + open.spotify.com/playlist/0ZA84BmrfbPD4PNQGCdTRh?si=e06ec2906f06400d + '> i made u a playlist <3 </a>';
   $(this).toggleClass("on");
 });
 
