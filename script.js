f69b5d059c7c27920102c0ef1377f899f13f5f64
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2021-09-10 22:03:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,45 @@
-//Problem: No user interacyion causes no change to application
-//Solution: User interaction causes changes appropriately
-
-var color = $(".selected").css("background-color");
-var $canvas = $("canvas");
-var context = $canvas[0].getContext("2d");
-var lastEvent;
-var mouseDown = false;
-
-//When clicking on control list items
-$(".controls").on("click", "li", function() {
-  //Deselect sibling elements
-  $(this).siblings().removeClass("selected");
-  //Select clicked element
-  $(this).addClass("selected");
-  //cache current color
-  color = $(this).css("background-color");
+$(document).ready(function() {
+  $("#Message").on("click", function(){
+   $("#target").fadeToggle(500).fadeToggle(500);
+  //  $(".message").html("Here is the message ");
+   
+  });
 });
 
-//When "New Color" is pressed
-$("#revealColorSelect").click(function() {
-  //Show color select or hide the color select
-  changeColor();
-  $("#colorSelect").toggle();
-});
-
-//update the new color span
-function changeColor() {
-  var r = $("#red").val();
-  var g = $("#green").val();
-  var b = $("#blue").val();
-  $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
+var showText = function (target, message, index, interval) {   
+if (index < message.length) {
+  $(target).append(message[index++]);
+  setTimeout(function () { showText(target, message, index, interval); }, interval);
+}
 }
 
-//When color sliders change
-$("input[type=range]").change(changeColor);
+$(function () {
 
-//When "Add Color" is pressed
-$("#addNewColor").click(function() {
-  //Append the color to the controls ul
-  var $newColor = $("<li></li>");
-  $newColor.css("background-color", $("#newColor").css("background-color"));
-  $(".controls ul").append($newColor);
-  //Select the new color
-  $newColor.click();
+showText("#target1", "-by Paul Newman Mekala", 0, 200);   
+
 });
 
-//On mouse events on the canvas
-$canvas.mousedown(function(e) {
-  lastEvent = e;
-  mouseDown = true;
-}).mousemove(function(e) {
-  //Draw lines
-  if (mouseDown) {
-    context.beginPath();
-    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-    context.lineTo(e.offsetX, e.offsetY);
-    context.strokeStyle = color;
-    context.stroke();
-    lastEvent = e;
-  }
-}).mouseup(function() {
-  mouseDown = false;
-}).mouseleave(function() {
-  $canvas.mouseup();
+$('button').click(function(){
+randomQuote();
 });
+function randomQuote() {
+var $target= $("#target");
+var $tweet= $("#tweet"); 
+$.ajax({
+  
+    url: "https://api.forismatic.com/api/1.0/?",
+    dataType: "jsonp",
+    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+    success: function( response ) {
+      $target.html("<h4 id='my' class='text-center'>" +
+        response.quoteText + "</h4><p id='para'><br>&dash; " + response.quoteAuthor + "</p> <br>      ");
+      
+      
+    }  
+});
+}
+ function changeLink(){
+     var textToTweet=$("#my").text()+ ' (' + $("#para").text()+')';
+   var tweetLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textToTweet);
+window.open(tweetLink,'_blank');
+ }
